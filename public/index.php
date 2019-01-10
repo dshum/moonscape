d83e85dde288a8790d308ae284d1fee644f410dc
Denis Shumeev <denis-shumeev@yandex.ru>
2019-01-10 19:08:58 +0300

    Some stuff is added.

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Moonscape</title>
+	<link href="/favicon.ico" type="image/x-icon" rel="shortcut icon">
+	<link href="/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/css/default.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script>
+		$(function() {
+			
+		});
+	</script>
+</head>
+<body>
+	<div class="container">
+        <h1>Hello!</h1>
+        <p>You're here.</p>
+        <h3>But there is nothing here at all...</h3>
+        <p>Today is <?=date('m/d/Y')?>.</p>
+        <h2>Have a good day.</h2>
+	</div>
+</body>
+</html>
